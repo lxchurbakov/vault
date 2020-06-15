b45f08f1d6b6c3d06c17e47b2e3d4e0d29f89b36
mcfinley <mcfinley@yandex.ru>
2020-06-15 15:17:35 +0300

    Update the playground

--- playground/encrypt-files/index.js
+++ playground/encrypt-files/index.js
@@ -7,17 +7,17 @@ const key = `14189dc35ae35e75ff31d7502e245cd9bc7803838fbfd5c773cdcd79b8a28bbd`.s
 const cipher = crypto.createCipheriv('aes-256-cbc', key, initVect)
 const decipher = crypto.createDecipheriv('aes-256-cbc', key, initVect)
 
-// fs.createReadStream('./input.md')
-//   .pipe(cipher)
-//   .pipe(fs.createWriteStream('./encrypted'))
+fs.createReadStream('./input.md')
+  .pipe(cipher)
+  .pipe(fs.createWriteStream('./encrypted'))
 
 // fs.createReadStream('./encrypted')
 //   .pipe(decipher)
 //   .pipe(fs.createWriteStream('./output.md'))
 
-fs.createReadStream('./input.md')
-  .pipe(cipher)
-  .pipe(decipher)
-  .pipe(fs.createWriteStream('./output.md'))
+// fs.createReadStream('./input.md')
+//   .pipe(cipher)
+//   .pipe(decipher)
+//   .pipe(fs.createWriteStream('./output.md'))
 
 /* TODO: looks like chunk sizes do not match on the first line - may need to reinvent the transforming pipe */
