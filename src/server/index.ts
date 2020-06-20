72b2c3f96af7e370582314139c9154855c76cd1f
mcfinley <mcfinley@yandex.ru>
2020-06-20 12:19:18 +0300

    Start implementing routes

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,20 +1,30 @@
 import express from 'express'
+import bodyParser from 'body-parser'
+
 import vaults from './vaults'
+import database from './database'
 
 /* Create the application */
-
 const app = express()
 
-app.get('/health', (req, res) => {
-  res.status(200).send('OK')
+app.use(bodyParser.json())
+
+app.get('/health', async (req, res) => {
+  res.status(200).json({
+    database: await database.health(),
+  })
 })
 
 /* Vaults router */
-
 app.use('/vaults', vaults)
 
+app.use((err, req, res, next) => {
+  if (!!err.code) {
+    res.status(err.code).send(err.toString())
+  } else {
+    res.status(500).send(err.toString())
+  }
+})
+
 /* Startup */
-
 app.listen(8500)
-
-export {}
