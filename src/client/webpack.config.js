144aed7922400e2825c00e057256e2af30204dd7
mcfinley <mcfinley@yandex.ru>
2020-07-06 12:15:05 +0300

    Setup webpack for client side

---
+++ src/client/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path')
+
+module.exports = {
+  context: __dirname,
+  entry: './index.tsx',
+
+  output: {
+    path: path.resolve(__dirname, '../../dist/client'),
+    filename: 'bundle.js',
+  },
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
+    },
+    extensions: [
+      '.ts', '.js', '.tsx', '.json'
+    ],
+  },
+
+  module: {
+    rules: [
+      { test: /\.ts/, loader: 'ts-loader' },
+      { test: /\.html$/, loader: 'file-loader?name=[name].html' },
+      { test: /\.png/, loader: 'file-loader?name=[name].png' },
+    ],
+  },
+}
