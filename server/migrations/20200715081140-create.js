cb701c4c36b7544927167fe8ee0c7b48acc7fc2a
mcfinley <mcfinley@yandex.ru>
2020-07-15 11:50:00 +0300

    Create real migrations

--- server/migrations/20200715081140-create.js
+++ server/migrations/20200715081140-create.js
@@ -14,26 +14,42 @@ exports.setup = function(options, seedLink) {
   seed = seedLink;
 };
 
-exports.up = (db, callback) => {
+exports.up = function(db, callback) {
   db.runSql(`
-    CREATE TABLE users (
+    CREATE TABLE vaults (
       id INT NOT NULL AUTO_INCREMENT,
       name VARCHAR(100) NOT NULL,
+      token VARCHAR(100) NOT NULL,
+      password_hash VARCHAR(100) NOT NULL,
       created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
       PRIMARY KEY(id)
-    )
+    );
+    CREATE UNIQUE INDEX vault_token ON vaults (token);
   `, callback)
 };
 
-exports.down = (db, callback) => {
+exports.down = function(db, callback) {
   db.runSql(`
-    DROP TABLE IF EXISTS users
+    DROP INDEX vault_token ON vaults;
+    DROP TABLE IF EXISTS vaults;
   `, callback)
 };
-
 
 exports._meta = {
   "version": 1
 };
 
+/*
 
+  CREATE TABLE file (
+    id INT NOT NULL,
+    PRIMARY KEY (id),
+    vault_id INT NOT NULL,
+    pathname VARCHAR(30) NOT NULL,
+    INDEX (vault_id),
+    FOREIGN KEY (vault_id)
+      REFERENCES vault(id)
+      ON DELETE CASCADE
+  );
+
+*/
